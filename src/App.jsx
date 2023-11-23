107621366001027218bdb722d1e8877086b3cd6d
rashednyn <rashedmahamud22@gmail.com>
2023-11-24 01:29:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>      
+      <p className="text-5xl">
+        Final Firefly
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">Main Layout</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
